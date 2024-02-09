c1a60ab2e0aa33febfdb1a7846eb9b05ea754d0a
divyendu <divyendua@gmail.com>
2024-02-08 23:27:12 -0500

    final

--- public/Kanbas/Navigation/index.js
+++ public/Kanbas/Navigation/index.js
@@ -64,3 +64,69 @@ function KanbasNavigation(name) {
       </ul>
         `;
 }
+function KanbasNavigationMin(name) {
+  const links = [
+    {
+      name: " ",
+      url: "http://northeastern.edu",
+      icon: "fa-solid fa-n",
+    },
+    {
+      name: "Account",
+      url: "/Kanbas/Account/Profile/screen.html",
+      icon: "fa-regular fa-user",
+    },
+    {
+      name: "Dashboard",
+      url: "/Kanbas/Dashboard/screen.html",
+      icon: "fa-solid fa-tachometer",
+    },
+    {
+      name: "Courses",
+      url: "/Kanbas/Courses/Home/screen.html",
+      icon: "fa-solid fa-book",
+    },
+    {
+      name: "Calendar",
+      url: "#",
+      icon: "fa-solid fa-calendar-days",
+    },
+    {
+      name: "Inbox",
+      url: "#",
+      icon: "fa-solid fa-envelope-open-text",
+    },
+    {
+      name: "History",
+      url: "#",
+      icon: "fa-regular fa-clock",
+    },
+    {
+      name: "Studio",
+      url: "#",
+      icon: "fa-solid fa-computer",
+    },
+    {
+      name: "Commons",
+      url: "#",
+      icon: "fa-brands fa-creative-commons-sa",
+    },
+    {
+      name: "Help",
+      url: "#",
+      icon: "fa-regular fa-circle-question",
+    },
+  ];
+  return `
+        <ul class="wd-navigation">
+        ${links
+          .map((link) => {
+            const isActive = link.name === name ? 'wd-active' : '';
+            return `
+            <li class="${isActive}"><a href="${link.url}"><i class="${link.icon}"></i> ${link.name}</a></li>
+            `;
+          })
+          .join("")}
+      </ul>
+        `;
+}
