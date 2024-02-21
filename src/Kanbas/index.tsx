b3fed4d05cafd18c5df54a6035813ccf2bf8f969
divyendu <divyendua@gmail.com>
2024-02-21 03:10:47 -0500

    lab

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,11 @@
+import Nav from "../Nav";
+
+function Kanbas() {
+   return (
+      <div className="container-fluid">
+         <h1>Kanbas</h1>
+         <Nav/>
+      </div>
+   );
+}
+export default Kanbas
