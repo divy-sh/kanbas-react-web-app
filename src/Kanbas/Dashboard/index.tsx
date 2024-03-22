0d4d2654f2a3963872a99f44c82b54b17a9c9a75
divyendu <divyendua@gmail.com>
2024-03-21 22:37:17 -0400

    fix dashboard style

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -4,8 +4,8 @@ function Dashboard(
     { courses, course, setCourse, addNewCourse, deleteCourse, updateCourse }: {
         courses: any[]; course: any; setCourse: (course: any) => void;
         addNewCourse: () => void; deleteCourse: (course: any) => void;
-        updateCourse: () => void; })
-    {
+        updateCourse: () => void;
+    }) {
 
 
     return (
@@ -13,21 +13,34 @@ function Dashboard(
             <h1>Dashboard</h1>
             <hr />
             <h5>Course</h5>
-            <input value={course.name} className="form-control"
-                onChange={(e) => setCourse({ ...course, name: e.target.value })} />
-            <input value={course.number} className="form-control"
-                onChange={(e) => setCourse({ ...course, number: e.target.value })} />
-            <input value={course.startDate} className="form-control" type="date"
-                onChange={(e) => setCourse({ ...course, startDate: e.target.value })} />
-            <input value={course.endDate} className="form-control" type="date"
-                onChange={(e) => setCourse({ ...course, endDate: e.target.value })} />
-
-            <button onClick={addNewCourse} >
-                Add
-            </button>
-            <button onClick={updateCourse} >
-                Update
-            </button>
+            <div className="row">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h5>Course Name</h5>
+                        <input value={course.name} className="form-control"
+                            onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+                        <h5>Course Number</h5>
+                        <input value={course.number} className="form-control"
+                            onChange={(e) => setCourse({ ...course, number: e.target.value })} />
+                    </div>
+                    <div className="col-md-6">
+                        <h5>Start Date</h5>
+                        <input value={course.startDate} className="form-control" type="date"
+                            onChange={(e) => setCourse({ ...course, startDate: e.target.value })} />
+                        <h5>End Date</h5>
+                        <input value={course.endDate} className="form-control" type="date"
+                            onChange={(e) => setCourse({ ...course, endDate: e.target.value })} />
+                    </div>
+                    <div>
+                    <button className="btn btn-primary float-end" onClick={addNewCourse} >
+                        Add
+                    </button>
+                    <button className="btn btn-secondary float-end" onClick={updateCourse} >
+                        Update
+                    </button>
+                </div>
+                </div>
+            </div>
             <h2>Published Courses ({courses.length})</h2> <hr />
             <div className="row">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
