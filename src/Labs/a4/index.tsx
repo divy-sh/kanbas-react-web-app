bd2ab7b3f4dc903804ae26de694cd2850a775f4e
divyendu <divyendua@gmail.com>
2024-03-21 22:48:31 -0400

    lab fix

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import PassingFunctions from "./PassingFunctions";
 import ReduxExamples from "./ReduxExamples";
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariable";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import StringStateVariables from "./StringStateVariable";
+import DateStateVariable from "./DateStateVariable";
 
 const Assignment4 = () => {
     function sayHello() {
@@ -11,7 +21,17 @@ const Assignment4 = () => {
     <>
       <h1>Assignment 4</h1>
       <ReduxExamples/>
-      <PassingFunctions theFunction={sayHello} />
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello}/>
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>      
     </>
   );
 };
